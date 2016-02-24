4bccadb6250c0b97da1e4b69ed0c92d3716f7b6a
exAspArk <exaspark@gmail.com>
2016-02-24 16:06:32 +0200

    Fix getting changlog by using $CURRENT_VERSION

--- modules/notify-slack.sh
+++ modules/notify-slack.sh
@@ -24,13 +24,9 @@ USER=`git config user.name`
 SLACK_MESSAGE="\`$COMMAND/$CURRENT_VERSION\` $ACTION by $USER"
 
 if [[ $ACTION == "finished" ]]; then
-  VERSION_CURRENT=$(__get_current_version)
-
-  VERSION_PREFIX=$(git config --get gitflow.prefix.versiontag)
-  VERSION="$VERSION_PREFIX$VERSION_CURRENT"
   PREV_VERSION=$(git describe --abbrev=0 --tags $(git rev-list --tags --max-count=2) | $VERSION_SORT -V | head -1)
 
-  CHANGES=$(git log --no-merges --pretty=format:"%s (%an)\n" "$VERSION"..."$PREV_VERSION")
+  CHANGES=$(git log --no-merges --pretty=format:"%s (%an)\n" "$CURRENT_VERSION"..."$PREV_VERSION")
   SLACK_MESSAGE="$SLACK_MESSAGE\n\nChanges:\n$CHANGES"
 fi
 
